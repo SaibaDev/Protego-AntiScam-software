559e51da01f3309d82f80162504a14b2114ef3c4
Syber Blaise Tolledo <70807433+SaibaDev@users.noreply.github.com>
2023-11-18 10:02:17 +0800

    Update dataCleaning.py

--- Python/dataCleaning.py
+++ Python/dataCleaning.py
@@ -1,43 +1,54 @@
-#Dinideconstruct yung URL into its different components para mas maayos kapag pinasok sa model
-
 import csv
-from urllib.parse import urlparse
+from urllib.parse import urlparse, parse_qs
 
 
-input_csv_path = 'Datasets\Dataset1.csv'
-output_csv_path = 'cleanedData1.csv'
+input_csv_path = 'Datasets\Dataset4.csv'
+output_csv_path = 'cleanedData4.csv'
 
-# Mga different components ng url
+#listt
 schemes = []
 netlocs = []
 paths = []
 params_list = []
 queries = []
 fragments = []
+labels = []
 
 
 with open(input_csv_path, 'r', newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
-    
-    for row in reader:
-        url = row['url']
-        parsed_url = urlparse(url)
-        
-        
-        schemes.append(parsed_url.scheme)
-        netlocs.append(parsed_url.netloc)
-        paths.append(parsed_url.path)
-        params_list.append(parsed_url.params)
-        queries.append(parsed_url.query)
-        fragments.append(parsed_url.fragment)
 
-#nilalagay ang mga output sa csv file
-with open(output_csv_path, 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['scheme', 'netloc', 'path', 'params', 'query', 'fragment']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     
-    writer.writeheader()
-    
-    
-    for components in zip(schemes, netlocs, paths, params_list, queries, fragments):
-        writer.writerow(dict(zip(fieldnames, components)))
+    with open(output_csv_path, 'w', newline='', encoding='utf-8') as output_csvfile:
+        fieldnames = ['scheme', 'netloc', 'path', 'params', 'query', 'fragment', 'label']
+        writer = csv.DictWriter(output_csvfile, fieldnames=fieldnames)
+        
+        
+        writer.writeheader()
+
+        for row in reader:
+            url = row['url']
+            label = row['label']
+            parsed_url = urlparse(url)
+
+            
+            schemes.append(parsed_url.scheme)
+            netlocs.append(parsed_url.netloc)
+            paths.append(parsed_url.path)
+            params_list.append(parse_qs(parsed_url.query))
+            queries.append(parsed_url.query)
+            fragments.append(parsed_url.fragment)
+            labels.append(label)
+
+            
+            writer.writerow({
+                'scheme': parsed_url.scheme,
+                'netloc': parsed_url.netloc,
+                'path': parsed_url.path,
+                'params': parse_qs(parsed_url.query),
+                'query': parsed_url.query,
+                'fragment': parsed_url.fragment,
+                'label': label
+            })
+
+
