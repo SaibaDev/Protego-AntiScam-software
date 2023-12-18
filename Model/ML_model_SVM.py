765f23b539bdb0fa6f791a2ded67af168521f5c9
Syber Blaise Tolledo <70807433+SaibaDev@users.noreply.github.com>
2023-12-18 19:58:41 +0800

    Update ML_model_SVM.py

--- Model/ML_model_SVM.py
+++ Model/ML_model_SVM.py
@@ -1,12 +1,38 @@
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import VotingClassifier
+import joblib
+import logging
 
-# Assuming 'label' is the column representing the target variable
-X = final_dataset[feature_columns]
-y = final_dataset['label']  # Replace 'label' with your actual target variable name
+logging.basicConfig(level=logging.DEBUG)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+file_path = 'Extracted_features\parsed_Fextracted1.csv'  
+data = pd.read_csv(file_path)
+
+
+features = data.drop(['url', 'label'], axis=1)  
+labels = data['label']
+logging.debug("#Extract features and labels")
+
+
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+logging.debug("# Split the data into training and testing sets")
+
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+logging.debug("# Standardize the features (optional but often improves performance)")
+
+
+rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_model.fit(X_train, y_train)
+logging.debug(" Train Random Forest Classifier")
+
 
 
